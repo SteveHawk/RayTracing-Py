7f88f5240fecf7df11eca5c1f6a0f0a4b7bd7142
SteveHawk <stevehawk327@gmail.com>
2020-05-25 01:08:42 +0800

    major improvement

--- InOneWeekend_optimized/utils/sphere.py
+++ InOneWeekend_optimized/utils/sphere.py
@@ -25,12 +25,23 @@ class Sphere(Hittable):
         c: np.ndarray = (oc**2).sum(axis=1) - self.radius**2
         discriminant_list: np.ndarray = half_b**2 - a*c
 
-        positive_discriminant_list = np.clip(discriminant_list, 0, np.inf)
+        discriminant_condition = discriminant_list > 0
+        if not discriminant_condition.any():
+            return HitRecordList(
+                np.empty((len(r), 3), dtype=np.float32),
+                np.zeros(len(r)),
+                [None for i in range(len(r))],
+                np.empty((len(r), 3), dtype=np.float32),
+                np.empty(len(r), dtype=np.bool)
+            )
+
+        positive_discriminant_list = (
+            discriminant_list * discriminant_condition
+        )
         root = np.sqrt(positive_discriminant_list)
         t_0 = (-half_b - root) / a
         t_1 = (-half_b + root) / a
 
-        discriminant_condition = discriminant_list > 0
         t_0_condition = (
             (t_min < t_0) & (t_0 < t_max_list) & discriminant_condition
         )
@@ -38,7 +49,7 @@ class Sphere(Hittable):
             (t_min < t_1) & (t_1 < t_max_list)
             & (~t_0_condition) & discriminant_condition
         )
-        t = np.where(t_0_condition, t_0, -1)
+        t = np.where(t_0_condition, t_0, 0)
         t = np.where(t_1_condition, t_1, t)
 
         point = r.at(t)
