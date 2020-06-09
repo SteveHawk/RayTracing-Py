9303fc2e538ebf1a7a0f93187b06c40bcb3e0a65
SteveHawk <stevehawk327@gmail.com>
2020-06-09 20:58:47 +0800

    implemented aabb class

---
+++ TheNextWeek_general/utils/aabb.py
@@ -0,0 +1,44 @@
+from utils.vec3 import Vec3, Point3
+from utils.ray import Ray
+
+
+class AABB:
+    def __init__(self, _min: Point3 = Point3(),
+                 _max: Point3 = Point3()) -> None:
+        self._min = _min
+        self._max = _max
+
+    def min(self) -> Point3:
+        return self._min
+
+    def max(self) -> Point3:
+        return self._max
+
+    def hit_deprecated(self, r: Ray, tmin: float, tmax: float) -> bool:
+        for i in range(3):
+            t0: float = min(
+                (self.min()[i] - r.origin()[i]) / r.direction()[i],
+                (self.max()[i] - r.origin()[i]) / r.direction()[i]
+            )
+            t1: float = min(
+                (self.min()[i] - r.origin()[i]) / r.direction()[i],
+                (self.max()[i] - r.origin()[i]) / r.direction()[i]
+            )
+            tmin = max(t0, tmin)
+            tmax = min(t1, tmax)
+            if tmax <= tmin:
+                return False
+        return True
+
+    def hit(self, r: Ray, tmin: float, tmax: float) -> bool:
+        for i in range(3):
+            invD: float = 1 / r.direction()[i]
+            t0: float = (self.min()[i] - r.origin()[i]) * invD
+            t1: float = (self.max()[i] - r.origin()[i]) * invD
+            if invD < 0:
+                t0, t1 = t1, t0
+            tmin = max(t0, tmin)
+            tmax = min(t1, tmax)
+            if tmax <= tmin:
+                return False
+        return True
