e693bb2da6964a8b9bb56a4d99b7c76f673351d0
SteveHawk <stevehawk327@gmail.com>
2020-06-10 20:32:26 +0800

    bvh implemented

--- TheNextWeek_general/utils/bvh.py
+++ TheNextWeek_general/utils/bvh.py
@@ -1,15 +1,38 @@
-from typing import Optional
+from typing import Optional, List, Callable
 from utils.hittable import Hittable, HitRecord
 from utils.ray import Ray
 from utils.aabb import AABB
 from utils.hittable_list import HittableList
+from utils.rtweekend import random_int
 
 
 class BVHNode(Hittable):
-    def __init__(self, hlist: HittableList, time0: float, time1: float):
-        self.left: Hittable
-        self.right: Hittable
-        self.box: AABB
+    def __init__(self, objects: List[Hittable], time0: float, time1: float):
+        axis = random_int(0, 2)
+        key_func: Callable[[Hittable], float] = self.key_func(axis)
+
+        length = len(objects)
+        if length == 1:
+            self.left = self.right = objects[0]
+        elif length == 2:
+            if key_func(objects[0]) < key_func(objects[1]):
+                self.left = objects[0]
+                self.right = objects[1]
+            else:
+                self.left = objects[1]
+                self.right = objects[0]
+        else:
+            sorted_objects = sorted(objects, key=key_func)
+            mid = int(length / 2)
+            self.left = BVHNode(sorted_objects[0:mid], time0, time1)
+            self.right = BVHNode(sorted_objects[mid:length], time0, time1)
+
+        box_left = self.left.bounding_box(time0, time1)
+        box_right = self.right.bounding_box(time0, time1)
+        if box_left is None or box_right is None:
+            print("No bounding box in bvh_node constructor.")
+            raise ValueError
+        self.box = AABB.surrounding_box(box_left, box_right)
 
     def hit(self, r: Ray, t_min: float, t_max: float) -> Optional[HitRecord]:
         if not self.box.hit(r, t_min, t_max):
@@ -24,3 +47,15 @@ class BVHNode(Hittable):
 
     def bounding_box(self, t0: float, t1: float) -> Optional[AABB]:
         return self.box
+
+    @staticmethod
+    def key_func(axis: int) -> Callable[[Hittable], float]:
+
+        def key(h: Hittable) -> float:
+            box: Optional[AABB] = h.bounding_box(0, 0)
+            if box is None:
+                print("No bounding box in bvh_node constructor.")
+                raise ValueError
+            return box.min().e[axis]
+
+        return key
--- TheNextWeek_general/utils/rtweekend.py
+++ TheNextWeek_general/utils/rtweekend.py
@@ -11,6 +11,10 @@ def random_float(_min: float = 0, _max: float = 1) -> float:
     return rng.uniform(_min, _max)
 
 
+def random_int(_min: int, _max: int) -> int:
+    return rng.integers(_min, _max + 1)
+
+
 def random_float_list(size: int, _min: float = 0, _max: float = 1) \
         -> np.ndarray:
     return rng.uniform(_min, _max, size)
